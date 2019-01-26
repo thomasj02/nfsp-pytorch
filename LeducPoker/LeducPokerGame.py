aad036f66360828a41187317b48ee390083c110a
Thomas Johnson <thomas.j.johnson@gmail.com>
2019-01-26 09:37:25 -0600

    Handle board cards in Leduc

--- LeducPoker/LeducPokerGame.py
+++ LeducPoker/LeducPokerGame.py
@@ -10,33 +10,56 @@ class PlayerActions:
 
 
 class LeducNode(object):
-    def __init__(self, bet_sequences: List[Tuple], board_card: Optional[int] = None):
+    def __init__(
+            self,
+            bet_sequences: List[Tuple],
+            hidden_board_card: Optional[int] = None,
+            board_card: Optional[int] = None):
         assert len(bet_sequences) == 2
-        self.bet_sequences = bet_sequences
+        assert (hidden_board_card is not None) or (board_card is not None)
+        assert len(bet_sequences[1]) == 0 or (board_card is not None)  # On first round, or board card showing
+        assert len(bet_sequences[1]) > 0 or (board_card is None)  # On second round, or board card hidden
+        self._bet_sequences = bet_sequences
+        self._hidden_board_card = hidden_board_card
         self.board_card = board_card
+        self.game_round = 0
+
+    @property
+    def bet_sequences(self):
+        return self._bet_sequences
 
     @property
     def is_terminal(self):
-        if len(self.bet_sequences[0]) > 0 and self.bet_sequences[0][-1] == PlayerActions.FOLD:
+        if len(self._bet_sequences[0]) > 0 and self._bet_sequences[0][-1] == PlayerActions.FOLD:
             return True
 
-        if len(self.bet_sequences[1]) <= 1:
+        if len(self._bet_sequences[1]) <= 1:
             return False
 
-        if self.bet_sequences[1][-1] != PlayerActions.BET_RAISE:
+        if self._bet_sequences[1][-1] != PlayerActions.BET_RAISE:
             return True
 
         return False
 
     @property
     def player_to_act(self):
-        return (len(self.bet_sequences[0]) + len(self.bet_sequences[1])) % 2
+        return (len(self._bet_sequences[0]) + len(self._bet_sequences[1])) % 2
+
+    def add_action(self, action: PlayerActions):
+        if self.game_round == 0:
+            if len(self.bet_sequences) >= 2 and self.bet_sequences[-1] == PlayerActions.CHECK_CALL:
+                self.board_card = self._hidden_board_card
+                self.game_round = 1
+            else:
+                self.bet_sequences[0] = self.bet_sequences[0] + (action,)
+        if self.game_round == 1:
+            self.bet_sequences[1] = self.bet_sequences[1] + (action,)
 
     def _get_half_pot(self):
         half_pot = 1  # Antes
 
         to_call = 0
-        for action in self.bet_sequences[0]:
+        for action in self._bet_sequences[0]:
             if action == PlayerActions.FOLD:
                 return half_pot
             elif action == PlayerActions.CHECK_CALL:
@@ -47,7 +70,7 @@ class LeducNode(object):
                 to_call = 2
 
         to_call = 0
-        for action in self.bet_sequences[1]:
+        for action in self._bet_sequences[1]:
             if action == PlayerActions.FOLD:
                 return half_pot
             elif action == PlayerActions.CHECK_CALL:
@@ -61,14 +84,14 @@ class LeducNode(object):
 
     def _get_winner(self, player_cards: List[int]):
         try:
-            fold_idx = self.bet_sequences[0].index(PlayerActions.FOLD)
+            fold_idx = self._bet_sequences[0].index(PlayerActions.FOLD)
             unfolded_player = (fold_idx + 1) % 2
             return unfolded_player
         except ValueError:
             pass
 
         try:
-            fold_idx = self.bet_sequences[1].index(PlayerActions.FOLD)
+            fold_idx = self._bet_sequences[1].index(PlayerActions.FOLD)
             unfolded_player = (fold_idx + 1) % 2
             return unfolded_player
         except ValueError:
@@ -106,15 +129,20 @@ class LeducInfoset(LeducNode):
         self.card = card
 
     def __eq__(self, other):
-        return (self.card == other.card and self.bet_sequences == other.bet_sequences
+        return (self.card == other.card and self._bet_sequences == other.bet_sequences
                 and self.board_card == other.board_card)
 
 
 class LeducGameState(LeducNode):
-    def __init__(self, player_cards: List[int], bet_sequences: List[Tuple], board_card: Optional[int] = None):
+    def __init__(
+            self,
+            player_cards: List[int],
+            bet_sequences: List[Tuple],
+            hidden_board_card: Optional[int],
+            board_card: Optional[int] = None):
         self.player_cards = player_cards
-        super().__init__(bet_sequences=bet_sequences, board_card=board_card)
-        self.infosets = tuple(LeducInfoset(card, self.bet_sequences) for card in self.player_cards)
+        super().__init__(bet_sequences=bet_sequences, hidden_board_card=hidden_board_card, board_card=board_card)
+        self.infosets = tuple(LeducInfoset(card, self._bet_sequences) for card in self.player_cards)
 
     def get_payoffs(self):
         return LeducNode.get_payoffs(self, self.player_cards)
@@ -122,7 +150,7 @@ class LeducGameState(LeducNode):
     def __setattr__(self, key, value):
         super().__setattr__(key, value)
         if key == "bet_sequences":
-            self.infosets = tuple(LeducInfoset(card, self.bet_sequences) for card in self.player_cards)
+            self.infosets = tuple(LeducInfoset(card, self._bet_sequences) for card in self.player_cards)
 
 
 class KuhnPokerGame(object):
@@ -130,7 +158,6 @@ class KuhnPokerGame(object):
         if player_cards is None:
             cards = random.sample(list(range(3)) + list(range(3)), 3)
             self.player_cards = cards[:2]
-            self.board_card = cards[-1]   # board card hidden to start
+            self.hidden_board_card = cards[-1]   # board card hidden to start
 
-        self.game_state = LeducGameState(player_cards, [(), ()])
-        # TODO: YOU ARE HERE, HANDLE BOARD CARD REVEAL
+        self.game_state = LeducGameState(player_cards, [(), ()], hidden_board_card=self.hidden_board_card)
--- LeducPoker/tests/LeducPokerGameTest.py
+++ LeducPoker/tests/LeducPokerGameTest.py
@@ -5,57 +5,57 @@ from LeducPoker.LeducPokerGame import PlayerActions
 class IsTerminalTest(unittest.TestCase):
     def test_game_start_is_not_terminal(self):
         bet_sequence = ()
-        self.assertFalse(LeducPokerGame.LeducNode([bet_sequence, ()], None).is_terminal)
+        self.assertFalse(LeducPokerGame.LeducNode([bet_sequence, ()], hidden_board_card=0).is_terminal)
 
     def test_p1_folds_is_terminal(self):
         bet_sequence = (PlayerActions.BET_RAISE, PlayerActions.FOLD)
-        self.assertTrue(LeducPokerGame.LeducNode([bet_sequence, ()], None).is_terminal)
+        self.assertTrue(LeducPokerGame.LeducNode([bet_sequence, ()], hidden_board_card=0).is_terminal)
 
     def test_p2_folds_is_terminal(self):
         bet_sequence = (PlayerActions.BET_RAISE, PlayerActions.BET_RAISE, PlayerActions.FOLD)
-        self.assertTrue(LeducPokerGame.LeducNode([bet_sequence, ()], None).is_terminal)
+        self.assertTrue(LeducPokerGame.LeducNode([bet_sequence, ()], hidden_board_card=0).is_terminal)
 
     def test_checkcall_showdown_is_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL)]
-        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, board_card=1).is_terminal)
 
     def test_raisecall_showdown_is_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             (PlayerActions.BET_RAISE, PlayerActions.CHECK_CALL)]
-        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, board_card=1).is_terminal)
 
     def test_checkraise_showdown_is_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             (PlayerActions.CHECK_CALL, PlayerActions.BET_RAISE, PlayerActions.CHECK_CALL)]
-        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertTrue(LeducPokerGame.LeducNode(bet_sequences, board_card=1).is_terminal)
 
     def test_checkraise_is_not_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             (PlayerActions.CHECK_CALL, PlayerActions.BET_RAISE)]
-        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, board_card=1).is_terminal)
 
     def test_check_is_not_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             (PlayerActions.CHECK_CALL,)]
-        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, board_card=1).is_terminal)
 
     def test_round1_is_not_terminal(self):
         bet_sequences = [
             (PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
             ()]
-        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, 1).is_terminal)
+        self.assertFalse(LeducPokerGame.LeducNode(bet_sequences, hidden_board_card=1).is_terminal)
 
 
 class PayoffsTest(unittest.TestCase):
     def test_nonterminal_payoff_raises_exception(self):
         with self.assertRaises(RuntimeError):
-            LeducPokerGame.LeducNode(bet_sequences=[(),()]).get_payoffs([0, 0])
+            LeducPokerGame.LeducNode(bet_sequences=[(),()], hidden_board_card=0).get_payoffs([0, 0])
 
     def test_check_it_down(self):
         bet_sequences = [(PlayerActions.CHECK_CALL, PlayerActions.CHECK_CALL),
@@ -80,21 +80,21 @@ class PayoffsTest(unittest.TestCase):
 
     def test_bet_fold(self):
         bet_sequences = [(PlayerActions.BET_RAISE, PlayerActions.FOLD), ()]
-        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences).get_payoffs([1, 2])
+        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences, hidden_board_card=0).get_payoffs([1, 2])
         self.assertEqual([1, -1], payoffs.tolist())
 
         bet_sequences = [(PlayerActions.CHECK_CALL, PlayerActions.BET_RAISE, PlayerActions.FOLD), ()]
-        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences).get_payoffs([2, 1])
+        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences, hidden_board_card=0).get_payoffs([2, 1])
         self.assertEqual([-1, 1], payoffs.tolist())
 
     def test_bet_raise_fold(self):
         bet_sequences = [(PlayerActions.BET_RAISE, PlayerActions.BET_RAISE, PlayerActions.FOLD), ()]
-        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences).get_payoffs([1, 2])
+        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences, hidden_board_card=0).get_payoffs([1, 2])
         self.assertEqual([-3, 3], payoffs.tolist())
 
         bet_sequences = [(PlayerActions.CHECK_CALL, PlayerActions.BET_RAISE, PlayerActions.BET_RAISE,
                                 PlayerActions.FOLD), ()]
-        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences).get_payoffs([2, 1])
+        payoffs = LeducPokerGame.LeducNode(bet_sequences=bet_sequences, hidden_board_card=0).get_payoffs([2, 1])
         self.assertEqual([3, -3], payoffs.tolist())
 
     def test_round1_bets_are_2(self):
